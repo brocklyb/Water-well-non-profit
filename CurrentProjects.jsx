d14f67a0b43ad44e4a92ae0699700321e0e4dc24
Brock B <52468736+brocklyb@users.noreply.github.com>
2024-01-31 18:01:36 -0700

    Add files via upload
    Created json file to access project data. Current projects.JSX can now access data and display contents

--- CurrentProjects.jsx
+++ CurrentProjects.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from 'react';
+
 import './styles/currentProjects.css'
 
+import projectData from './data/projectData.json'
+
 function CurrentProjects() {
+  const [displayData, setDisplayData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+
+  useEffect(() => {
+    setDisplayData(projectData)
+  }, []);
+
+
+  const filterData = () =>{
+    setFilteredData(displayData[1].location)
+  }
+
+
   return (
     <div>
-      <h1>Current Projects</h1>
+      <button onClick={filterData}>Filter Data</button>
+        <div>
+              {displayData.length > 0 ? (
+                displayData.map((dataItem, index) => (
+                  <div key={index}>
+                    <h1>{dataItem.location}</h1>
+             
+                  </div>
+                ))
+              ) : (
+                <p>No data available</p>
+              )}
+            </div>
+
+
+            <div>
+              {filteredData.length > 0 ? (
+                filteredData.map((dataItem, index) => (
+                  <div key={index}>
+                    <h1>{dataItem.location}</h1>
+             
+                  </div>
+                ))
+              ) : (
+                <p>No data available</p>
+              )}
+            </div>
+
     </div>
   );
 }
