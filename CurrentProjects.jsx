72c632c5381ae20510346ac2626fc79ef4baa2b1
Brock B <52468736+brocklyb@users.noreply.github.com>
2024-02-07 18:35:58 -0700

    Add files via upload
    Added counter to count amount of projects displayed. Created first step of filtering data.

--- CurrentProjects.jsx
+++ CurrentProjects.jsx
@@ -17,9 +17,13 @@ function CurrentProjects() {
 
   const [filteredData, setFilteredData] = useState([])
 
-        const showAllProject = () =>{
-          setFilteredData(projectData)
+  const [projectCounter, setProjectCounter] = useState(0)
+
+        const countData = () => {
+          const count = filteredData.length
+          setProjectCounter(count)
         }
+
 
         const handleLocationChange = (event) => {
           setprojectLocation(event.target.value);
@@ -35,12 +39,44 @@ function CurrentProjects() {
       
         const handleSubmit = (event) => {
           event.preventDefault();
+          //incase user does not use selection box on first render and clicks submit
+          if(projectLocation == ""){
+            setprojectLocation('ALL')
+          }
+          const allFilters = [projectLocation, projectCompletionDate, isProjectCompleted]
+          console.log(allFilters)
           filterData()
         };
 
         const filterData = () => {
-          const filteredData = projectData.filter(obj => obj.location === projectLocation);
-          setFilteredData(filteredData);
+          //const filteredData = projectData.filter(obj => obj.location === projectLocation);
+          //setFilteredData(filteredData);
+          
+          if(projectLocation == "ALL" && projectCompletionDate == "" && isProjectCompleted == false){
+            //const filterMe = projectData.filter(obj => obj.location === projectLocation);
+            setFilteredData(projectData); 
+
+          }else if(projectLocation == "ALL" && projectCompletionDate == "" && isProjectCompleted == true){
+            const filterMe1 = projectData.filter(obj => obj.isCompleted == isProjectCompleted);
+            console.log(filterMe1)
+            setFilteredData(filterMe1); 
+            console.log('ALL and True')
+
+          }else{
+            const filterMe2 = projectData.filter(obj => obj.location === projectLocation);
+            setFilteredData(filterMe2); 
+          }
+          
+          countData()
+
+
+          //NO completion date and sort = TRUE
+          //if (projectCompletionDate == "" && isProjectCompleted == true){
+            //const filteredData2 = projectData.filter(obj => obj.location === projectLocation && obj.isCompleted === isProjectCompleted);
+            //setFilteredData(filteredData2);  
+            //console.log('NO DATE sort TRUE => Filtered data below')
+            //console.log(filteredData2)
+          //}
         }
       
         return (
@@ -50,7 +86,7 @@ function CurrentProjects() {
             <form onSubmit={handleSubmit}>
               <label htmlFor="location">Location:</label>
               <select id="location" name="location" value={projectLocation} onChange={handleLocationChange}>
-                <option value={projectData}>Show All Projects</option>
+                <option value='ALL'>Show All Projects</option>
                 <option value="Ghana">Ghana</option>
                 <option value="Kenya">Kenya</option>
                 <option value="Sierra Leone">Sierra Leone</option>
@@ -63,7 +99,7 @@ function CurrentProjects() {
                 id="completionDate"
                 value={projectCompletionDate}
                 onChange={handleCompletionDateChange}>
-                <option>-</option>  
+                <option value="">-</option>  
                 <option value="2000">2000</option>
                 <option value="2001">2001</option>
                 <option value="2005">2005</option>
@@ -81,8 +117,12 @@ function CurrentProjects() {
               <br />
       
               <input type="submit" value="Submit" />
-              <button>Reset</button>
+        
             </form>
+
+            <div>
+              <h2>Project Count = {projectCounter}</h2>
+            </div>
 
             <hr></hr>
 
@@ -95,17 +135,19 @@ function CurrentProjects() {
                   <h1>{item.projectName}</h1>
                   <h4>{item.location}</h4>
                   <p>{item.description}</p>
-                  <p>Goal: ${item.moneyGoal}</p> 
-                  <p>Current: ${item.moneyCurrent}</p>
 
-                  {item.moneyGoal <= item.moneyCurrent ? (
+                  {item.isCompleted == true ? (
                     <div id='isCompleted'>
                       <p id='completion'><strong>Completed</strong></p>
                       <p id='completion'>{item.completionYear}</p>
                       <img id='checkMark' src={checkMark}></img>
                     </div>
                   ) : (
-                    <p>Completion ETA: {item.completionYear}</p>
+                    <div>
+                      <p>Goal: ${item.moneyGoal}</p> 
+                      <p>Current: ${item.moneyCurrent}</p>
+                    </div>
+                    
                   )}
 
                   <a href=''>Learn More</a>
