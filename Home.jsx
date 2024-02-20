52d6d48ef30d6575f4304bcf7aea0a3da2301956
Brock B <52468736+brocklyb@users.noreply.github.com>
2024-02-20 15:06:27 -0700

    Add files via upload
    Added styling to projects slider and more content on the home page

--- Home.jsx
+++ Home.jsx
@@ -10,21 +10,39 @@ import Section3Img from './images/kids-well2.jpg'
 import SmilingChild from './images/smiling-kid.jpg'
 import BucketKids from './images/project-photo.jpg'
 
+import {projectData} from './data/Data'
+import checkMark from './images/icons/check-mark.svg'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react'
 
 function Home() {
+
+  const [ projects, setProjects ] = useState([])
+
+  useEffect(() => {
+    creatProjects()
+  }, [])
+
+  const creatProjects = () =>{
+    const projects = []
+    for(let i =0; i<15; i++){
+      projects.push(projectData[i])
+    }
+    setProjects(projects)
+  }
   
   return (
     <div>
 
       <div id='hero_container'>
         <img id='hero_card' src={HeroCard} />  
-        <div className='overlay-text'></div>
       </div>
 
       <div id='logo_container'>
         <img className='test' src={Logo}></img>
+      </div>
+      <div id='logo_container'>
         <h1 className='test'>The Water Project</h1>
       </div>
 
@@ -68,10 +86,63 @@ function Home() {
         </div>
       </div>
 
-      
+      <div id='cur-prjs'>
+        <h1>Current Projects</h1>
+      </div>
+
+
+          <div id='project-container'>
+            <div id='projects'>
+              
+              {projects.map((item,position) => (
+           
+                <div className='each-prj-card' key={position}>
+                <div className='project-details'>
+                  <div>
+                    <img src={item.image} alt='Failed to load'></img>
+                    <h1>{item.projectName}</h1>
+                  </div>
+
+                  <h4>{item.location}</h4>
+        
+
+                  {item.isCompleted == true ? (
+                    <div id='item4'>
+                      <p id='completion'><strong>Completed</strong></p>
+                      <p id='completion'>{item.completionYear}</p>
+                      <img id='checkMark' src={checkMark}></img>
+                      
+                    </div>
+                  ) : (
+                    <div id='item4'>
+                      <p>Goal: ${item.moneyGoal}</p> 
+                      <p>Current: ${item.moneyCurrent}</p>
+                      <p><strong>IN PROGRESS</strong></p>
+                    </div>
+                    
+                  )}
+
+                  <a id='item5' href=''>Learn More</a>
+                </div>
+                </div> 
+                  ))}
+  
+            </div>
+          </div>
+
+          <div id='google-api'>
+            <h2 id='google-api'>GOOGLE API HERE</h2>
+          </div>
+
+          <div id='newsletter'>
+            <h3>Sign up for our newsletter</h3>
+            <input placeholder='Email'></input>
+            <input placeholder='First Name'></input>
+            <input placeholder='Last Name'></input>
+            <button>Subscribe</button>
+          </div>
+
     <Footer />
-
-
     </div>
   );
 }
